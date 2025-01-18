6486be7e8e4b87093ea70d76fdc80e7f6fc4cd67
Roman Martynoff <nonsensr@gmail.com>
2025-01-18 09:49:50 +0300

    Complete exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,28 +3,30 @@ import Content from "./Content.jsx";
 import Total from "./Total.jsx";
 
 function App() {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const parts = [part1, part2, part3];
 
   return (
     <div>
-      <Header header={course} />
-      <Content parts={parts} />
+      <Header header={course.name} />
+      <Content parts={course.parts} />
       <hr/>
-      <Total parts={parts} />
+      <Total parts={course.parts} />
     </div>
   );
 }
